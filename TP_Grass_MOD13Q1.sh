ed6164b272fd559f4f395bfa0150a23e0e2faa66
micaredlich <82115312+micaredlich@users.noreply.github.com>
2021-04-20 10:09:39 -0300

    Update TP_Grass_MOD13Q1.sh

--- TP_Grass_MOD13Q1.sh
+++ TP_Grass_MOD13Q1.sh
@@ -37,7 +37,7 @@ t.register -i input=evi  maps=`g.list type=raster pattern="MOD13Q1*EVI*" separat
 
 # Create the STRDS for pixel QA and register
 
-t.create output=QA type=strds temporaltype=absolute title="QA 16 days" description="Calidad del pixel"
+t.create output=QA type=strds temporaltype=absolute title="QA 16 days" description="Pixel Quality"
 t.register -i input=QA  maps=`g.list type=raster pattern="MOD13Q1*VI_Quality*" separator=comma`  start="2020-01-01" increment="16 days"
 
 # Generate a mask for each bitcode flag
@@ -52,34 +52,34 @@ t.rast.mapcalc inputs=QA output=QA_f7 basename=QA_f7 expression="QA & 0x3800"
 t.rast.mapcalc inputs=QA output=QA_f8 basename=QA_f8 expression="QA & 0x4000" 
 t.rast.mapcalc inputs=QA output=QA_f9 basename=QA_f9 expression="QA & 0x8000"
 
-# Create a mask for each date using flags' information
+# Create a mask for each date using flag's information
 t.rast.mapcalc inputs=QA_f1,QA_f2,QA_f3,QA_f4,QA_f5,QA_f6,QA_f7,QA_f8,QA_f9 output=QA_mask basename=QA_mask expression="if(QA_f1 == 0 && QA_f2 < 20 && QA_f4 < 192 && QA_f6 == 0 && QA_f8 == 0 && QA_f9 ==  0, 0, 1)"
 
 # Mask low quality pixels in ndvi serie
 t.rast.mapcalc inputs=QA_mask,ndvi expression="if(QA_mask==0,ndvi,null())" output=ndvi_masked basename=ndvi_masked
 
 # Create a ndvi time series using spatial interpolation (by neighborhood analysis)
-#Vecindad
+#Neighborhood
 t.rast.neighbors input=ndvi output=ndvi_nb method=average basename=ndvi_nb size=3
 
 # Create a series of smoothed images from a moving average for the ndvi
 #Temporal
 t.rast.algebra expression="ndvi_smooth = 0.5*(ndvi[1]+ndvi[-1])" basename=ndvi_smooth
 
-# Create a series of images smoothed taking into account the spatio-temporal context for the ndvi
-#Espacio-temporal
+# Create a series of images smoothed taking into account the space-time context for the ndvi
+#Temporal-space
 t.rast.algebra expression="ndvi_smooth_spacetime=0.3*ndvi[1]+0.3*ndvi[-1]+0.10*(ndvi[0,-1]+ndvi[0,1]+ndvi[-1,0]+ndvi[1,0])" basename=ndvi_smooth_spacetime
 
 # Replace the masked pixels according to the "neighborhood" images
-# Por vecindad
+# Neighborhood
 t.rast.mapcalc inputs=QA_mask,ndvi,ndvi_nb expression="if(QA_mask==0,ndvi,ndvi_nb)" output=ndvi_filter_nb basename=ndvi_filter_nb
 
 # Replace pixels masked according to temporal moving average smoothing
-# Por temporal
+# Temporal
 t.rast.mapcalc inputs=QA_mask,ndvi,ndvi_smooth expression="if(QA_mask==0,ndvi,ndvi_smooth)" output=ndvi_filter_smooth basename=ndvi_filter_smooth
 
 # Replace the masked pixels according to the space-time context
-# Por espacio-temporal
+# Temporal-space
 t.rast.mapcalc inputs=QA_mask,ndvi,ndvi_smooth_spacetime expression="if(QA_mask==0,ndvi,ndvi_smooth_spacetime)" output=ndvi_filter_smooth_spacetime basename=ndvi_filter_smooth_spacetime
 
 ###REPEAT STEPS FOR EVI TIME SERIES###
